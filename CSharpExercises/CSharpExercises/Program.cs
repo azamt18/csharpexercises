911f7cef2016b55118da398624c12f5c534acef2
azam <azam.turgunboyev@gmail.com>
2020-12-15 08:38:11 +0500

    Initial program state

---
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial program state");
+
+            Console.ReadLine();
+        }
+    }
+}
