a505e0927c97d3e37c5021c150064fdf507bf45d
azam <azam.turgunboyev@gmail.com>
2021-01-05 02:43:19 +0500

    Solve new exercises on strings --- Complete the chapter

---
+++ CSharpExercises/CSharpExercises/Strings/HowManyOccurrences.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CSharpExercises.Strings
+{
+    class HowManyOccurrences
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HowManyOccurrencesMethod("do it now", "do")); //  → 1
+            Console.WriteLine(HowManyOccurrencesMethod("empty", "d")); //  → 0
+            Console.WriteLine(HowManyOccurrencesMethod("104 593 00 - 930 720193", "93")); //  → 3
+
+            Console.ReadKey();
+        }
+
+        static int HowManyOccurrencesMethod(string input, string pattern)
+        {
+            //var count = Regex.Matches(input, pattern).Count;
+            int count = 0;
+            int found = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                found = input.IndexOf(pattern, i);
+
+                if (found > -1)
+                {
+                    count++;
+                    i = found;
+                }
+            }
+
+            return count;
+        }
+    }
+}
--- CSharpExercises/CSharpExercises/Strings/NumberOfWords.cs
+++ CSharpExercises/CSharpExercises/Strings/NumberOfWords.cs
@@ -6,8 +6,14 @@ namespace CSharpExercises.Strings
     {
         static void Main(string[] args)
         {
+            //todo - this should return 0, now it returns 1
+            Console.WriteLine(NumberOfWordsMethod("")); // 0
+
             Console.WriteLine(NumberOfWordsMethod("This is sample sentence")); //  → 4
-            Console.WriteLine(NumberOfWordsMethod("OK")); //  → 4
+            Console.WriteLine(NumberOfWordsMethod("OK")); //  → 1
+            Console.WriteLine(NumberOfWordsMethod("Mauris consectetur urna sit amet risus ultricies rutrum.")); // 8
+            Console.WriteLine(NumberOfWordsMethod("Quisque M"));                                                // 2
+            Console.WriteLine(NumberOfWordsMethod("Xor"));
 
             Console.ReadKey();
         }
@@ -15,13 +21,12 @@ namespace CSharpExercises.Strings
         static int NumberOfWordsMethod(string str)
         {
             int counter = 0;
-            foreach (var c in str)
+            for (int i = 1; i < str.Length; i++)
             {
-                if (c == ' ')
-                    counter++;
+                counter = char.IsWhiteSpace(str[i]) ? counter + 1 : counter;
             }
 
-            return ++counter;
+            return counter + 1;
         }
     }
 }
