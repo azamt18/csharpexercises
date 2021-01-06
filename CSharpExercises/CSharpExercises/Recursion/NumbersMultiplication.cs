5c0379e6fdac03d6647164a887ff8d7f44f79357
Azam <azam.turgunboyev@gmail.com>
2021-01-06 13:57:57 +0500

    Solve 2 more exercises on Recursion

---
+++ CSharpExercises/CSharpExercises/Recursion/NumbersMultiplication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpExercises.Recursion
+{
+    public class NumbersMultiplication
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumbersMultiplicationMethod(5, 7)); // 210
+            Console.WriteLine(NumbersMultiplicationMethod(50, 50)); // 210
+
+            Console.ReadLine();
+        }
+
+        static int NumbersMultiplicationMethod(int a, int b)
+        {
+            while (a == b)
+            {
+                return a;
+            }
+
+            var result = a * NumbersMultiplicationMethod(a + 1, b);
+
+            return result;
+        }
+    }
+}
---
+++ CSharpExercises/CSharpExercises/Recursion/ToThePowerOfRecursion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CSharpExercises.Recursion
+{
+    public class ToThePowerOfRecursion
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToThePowerOfRecursionMethod(2,3)); // 8
+            Console.WriteLine(ToThePowerOfRecursionMethod(5,2)); // 25
+
+            Console.WriteLine(ToThePowerOfRecursionByRecursion(2,3)); // 8
+            Console.WriteLine(ToThePowerOfRecursionByRecursion(5,2)); // 25
+
+            Console.ReadLine();
+        }
+
+        // MY SOLUTION
+        static int ToThePowerOfRecursionMethod(int num, int power)
+        {
+            var result = 1;
+
+            for (int i = 1; i <= power; i++)
+            {
+                result *= num;
+            }
+
+            return result;
+        }
+
+        // AUTHOR'S SOLUTION
+        static int ToThePowerOfRecursionByRecursion(int num, int exp)
+        {
+            if (exp == 0)
+            {
+                return 1;
+            } 
+            else if (exp > 1)
+            {
+                return num * ToThePowerOfRecursionByRecursion(num, exp - 1);
+            }
+            else
+            {
+                return num;
+            }
+        }
+    }
+}
