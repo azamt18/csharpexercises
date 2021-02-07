598f818f20f028b26adbbe905ec02576405a5206
azam <azam.turgunboyev@gmail.com>
2021-02-07 17:45:28 +0500

    Solve one exercises on regular expression

---
+++ CSharpExercises/CSharpExercises/RegularExpressions/AlmostOnlyLetters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CSharpExercises.RegularExpressions
+{
+    class AlmostOnlyLetters
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(AlmostOnlyLettersMethod("Very hot."));      // True
+            Console.WriteLine(AlmostOnlyLettersMethod("full of hope"));   // False
+            Console.WriteLine(AlmostOnlyLettersMethod(""));               // False
+            Console.WriteLine(AlmostOnlyLettersMethod("short."));         // True
+            Console.ReadKey();
+        }
+
+        static bool AlmostOnlyLettersMethod(string str)
+        {
+            Regex regex = new Regex(@"^[A-Za-z\s]+\.$");
+            Match match = regex.Match(str);
+
+            return match.Success;
+        }
+            
+    }
+}
