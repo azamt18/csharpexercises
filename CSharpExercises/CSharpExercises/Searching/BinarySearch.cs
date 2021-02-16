3ef363f5bd74dfa49ec77b2b0f8e9b6aed599a84
Azam <azam.turgunboyev@gmail.com>
2021-02-16 20:13:51 +0500

    Apply searching algorithms

---
+++ CSharpExercises/CSharpExercises/Searching/BinarySearch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSharpExercises.Searching
+{
+    public class BinarySearch
+    {
+        static int BinarySearchRecursive(int[] array, int searchingElement, int leftmostIndex, int rightmostIndex)
+        {
+
+            if (leftmostIndex < rightmostIndex)
+            {
+                // find middle index
+                int middleIndex = leftmostIndex + (rightmostIndex - leftmostIndex) / 2;
+
+                if (array[middleIndex] == searchingElement)
+                    return middleIndex;
+
+                // If element is smaller than mid, then it can only be present in left sub-array 
+                if (array[middleIndex] > searchingElement)
+                    return BinarySearchRecursive(array, searchingElement, leftmostIndex, middleIndex - 1);
+
+                // Else the element can only be present in right sub-array 
+                return BinarySearchRecursive(array, searchingElement, middleIndex + 1, rightmostIndex);
+            }
+
+            return -1;
+        }
+
+        static int BinarySearchIterative(int[] array, int searchingElement)
+        {
+            int leftmostIndex = 0;
+            int rightmostIndex = array.Length - 1;
+            while (leftmostIndex <= rightmostIndex)
+            {
+                int middleIndex = leftmostIndex + (rightmostIndex - leftmostIndex) / 2;
+
+                if (array[middleIndex] == searchingElement)
+                    return middleIndex;
+
+                if (array[middleIndex] < searchingElement)
+                    leftmostIndex = middleIndex + 1;
+                else
+                    rightmostIndex = middleIndex - 1;
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 4, 10, 40 };
+            int n = arr.Length;
+            int x = 10;
+
+            int binarySearchRecursiveResult = BinarySearchRecursive(arr, x, 0, n - 1);
+            if (binarySearchRecursiveResult == -1)
+                Console.WriteLine("BinarySearchRecursive: Element not present");
+            else
+                Console.WriteLine("BinarySearchRecursive: Element found at index " + binarySearchRecursiveResult);
+
+            int binarySearchIterativeResult = BinarySearchIterative(arr, x);
+
+            if (binarySearchIterativeResult == -1)
+                Console.WriteLine("BinarySearchIterative: Element not present");
+            else
+                Console.WriteLine("BinarySearchIterative: Element found at index " + binarySearchIterativeResult);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpExercises/CSharpExercises/Searching/LinearSearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharpExercises.Searching
+{
+    public class LinearSearch
+    {
+        static int linearSearch(int[] array, int searchingElement)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == searchingElement)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 4, 10, 40 };
+            int x = 10;
+
+            // Function call 
+            int result = linearSearch(arr, x);
+            if (result == -1)
+                Console.WriteLine(
+                    "Element is not present in array");
+            else
+                Console.WriteLine("Element is present at index "
+                                  + result);
+
+            Console.ReadKey();
+        }
+    }
+}
